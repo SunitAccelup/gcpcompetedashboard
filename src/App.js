e0e3502f92bf9d5e7581ccba7a2a2ff55aa68cc9
sunitaccelup <randhawa@gmail.com>
2022-08-18 15:59:38 -0700

    changed table headers

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
   function getUser() {
     return Auth.currentAuthenticatedUser()
       .then((userData) => userData)
-      .catch(() => console.log("Not signed in"));
+      .catch(() => console.log("Not signed in!!!"));
   }
 
   // For form
